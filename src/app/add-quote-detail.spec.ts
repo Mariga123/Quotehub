c2af95ea8ff086ef2e6db5a59aa85a809c84720b
Mariga123 <johnmariga8@gmail.com>
2020-11-07 20:48:36 +0300

    generate a new component to be nested as child components to show quote details and authors

---
+++ src/app/add-quote-detail.spec.ts
@@ -0,0 +1,7 @@
+import { AddQuoteDetail } from './add-quote-detail';
+
+describe('AddQuoteDetail', () => {
+  it('should create an instance', () => {
+    expect(new AddQuoteDetail()).toBeTruthy();
+  });
+});
