739ad0a52144395ef1ee55d8b57b945fff551d09
Mariga123 <johnmariga8@gmail.com>
2020-11-09 10:10:04 +0300

    add media Querry

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,10 @@
 export class Quotes {
     public showDescription: boolean;
+    like: number;
+    dislike: number;
     constructor(public title: string, public description: string, public author: string, public completeDate: Date) {
         this.showDescription = false;
+        this.like = 0;
+        this.dislike = 0;
     }
 }
