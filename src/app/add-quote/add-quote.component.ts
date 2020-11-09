fcff509bf11aadc2ce56af4ccb175b517a876b7f
Mariga123 <johnmariga8@gmail.com>
2020-11-09 07:30:36 +0300

    edit add Quoe event listner to add to form

--- src/app/add-quote/add-quote.component.ts
+++ src/app/add-quote/add-quote.component.ts
@@ -9,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class AddQuoteComponent implements OnInit {
 
   quote: Quotes[] = [
-    new Quotes('You must be the change you wish to see in the world.', 'Live for what’s worth dying for, and leverage technology to create the world you wish to see.', 'Gandhi', '10/07/2121'),
-    new Quotes('A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing.', 'Experiments are necessary for the experiences that create growth and new opportunities.', 'George Bernhard Shaw', '10/06/2121'),
-    new Quotes('Love is a verb. Love — the feeling — is a fruit of love, the verb.', 'Love has been mistaken for possession and attachment. Become loving, so you can love all', 'Stephen Covey', '10/08/2221'),
-    new Quotes('In seeking happiness for others, you will find it in yourself.', 'Don’t search for what you’re passionate about, serve others to make yourself passionate.', 'John mariga', '01/02/2221'),
+    new Quotes('You must be the change you wish to see in the world.', 'Live for what’s worth dying for, and leverage technology to create the world you wish to see.', 'Gandhi', new Date(2000, 1, 15)),
+    new Quotes('A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing.', 'Experiments are necessary for the experiences that create growth and new opportunities.', 'George Bernhard Shaw', new Date(2010, 6, 21)),
+    new Quotes('Love is a verb. Love — the feeling — is a fruit of love, the verb.', 'Love has been mistaken for possession and attachment. Become loving, so you can love all', 'Stephen Covey', new Date(2020, 7, 25)),
+    new Quotes('In seeking happiness for others, you will find it in yourself.', 'Don’t search for what you’re passionate about, serve others to make yourself passionate.', 'John mariga', new Date(2014, 5, 15)),
 
   ];
+
+  // tslint:disable-next-line: typedef
+  addNewQuotes(goal: any){
+    const quoteLength = this.quote.length;
+    goal.id = quoteLength + 1;
+    goal.completeDate = new Date(goal.completeDate);
+    this.quote.push(goal);
+  }
 
   // tslint:disable-next-line: typedef
   completeQuote(isComplete, index){
